72f016a254cc39a83a17d145cef2babde48d3f6c
Ertan Kaya <107481123+ertantherock@users.noreply.github.com>
2022-08-14 23:33:09 +0300

    Add files via upload

---
+++ if_Else.js
@@ -0,0 +1,35 @@
+var a = 23;
+
+if (10<a<50) {
+    alert("a 10 ile 50 arasında");
+}
+else {
+    alert("a 10 ile 50 arasında değil");
+
+}
+
+
+let b = 33;
+if (b%2 == 0) {
+    alert(" Bu sayı tek sayı değildir.");
+} 
+else {
+    alert("Bu sayı tek sayıdır.");
+}
+
+
+let x = 30;
+let y = 50;
+let z = 45;
+
+if ((x>y) && (x>z)) {
+    alert("X y ve z den büyüktür.");
+}
+else if ((y>z) && (y>x)) {
+    alert("Y X ve Z'den büyüktür.")
+}
+else {
+    alert("Z hepsinden büyüktür.");
+}
+
+    
